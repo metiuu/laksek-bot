677568b5d2b2675d2b5b36124c1774f0d76f76d3
metiuu <101504749@student.swin.edu.au>
2020-05-13 15:49:07 +1000

    Moved forecast to cog

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ import discord
 from discord.ext import commands
 
 client = commands.Bot(command_prefix= '.')
+token = '===== INSERT YOUR TOKEN HERE ====='
 
 # cogs
 @client.command()
@@ -13,6 +14,11 @@ async def load(ctx, extension):
 @client.command()
 async def unload(ctx, extension):
     client.unload_extension(f'cogs.{extension}')
+
+@client.command()
+async def reload(ctx, extension):
+    client.unload_extension(f'cogs.{extension}')
+    client.load_extension(f'cogs.{extension}')
 
 # welcome message
 @client.event
@@ -28,27 +34,8 @@ async def on_member_remove(member):
 async def clear(ctx, amount=5):
     await ctx.channel.purge(limit=amount)
 
-# gacha forecast
-@client.command(aliases=['ramalan', 'forecast'])
-async def gacha_forecast(ctx, member : discord.Member):
-    responses = ["Rate up is a lie.",
-        "Ampas",
-        "YOROKOBE",
-        "SSR",
-        "SR",
-        "Waifu doesn't come home",
-        "Luck E",
-        "Prepare for salt",
-        "Pouring salt for others",
-        "SSR in spare account",
-        "Just give up",
-        "Credit card to the rescue!",
-        "Single roll is the way",
-        "Spook incoming"]
-    await ctx.send(f'Forecast for: {member.mention}\nAnswer: {random.choice(responses)}')
-
 for filename in os.listdir('./cogs'):
     if(filename.endswith('.py')):
         client.load_extension(f'cogs.{filename[:-3]}')
 
-client.run('====== INSERT YOUR TOKEN HERE ======')
+client.run(token)
--- cogs/laksek.py
+++ cogs/laksek.py
@@ -9,12 +9,31 @@ class Laksek(commands.Cog):
     # Events
     @commands.Cog.listener()
     async def on_ready(self):
+        await client.change_presence(status=discord.Status.online, activity=discord.Game('Fate/Grand Order'))
         print('Gudako on standby.')
     
     # Commands
     @commands.command()
     async def callout(self, ctx, member : discord.Member):
         await ctx.send(f'{member.mention} is a laksek!')
+    
+    @commands.command(aliases=['ramalan', 'forecast'])
+    async def gacha_forecast(ctx, member : discord.Member):
+    responses = ["Rate up is a lie.",
+        "Ampas",
+        "YOROKOBE",
+        "SSR",
+        "SR",
+        "Waifu doesn't come home",
+        "Luck E",
+        "Prepare for salt",
+        "Pouring salt for others",
+        "SSR in spare account",
+        "Just give up",
+        "Credit card to the rescue!",
+        "Single roll is the way",
+        "Spook incoming"]
+    await ctx.send(f'Forecast for: {member.mention}\nAnswer: {random.choice(responses)}')
 
 def setup(client):
     client.add_cog(Laksek(client))
